b8e74a8ade9b1ada178d900c2cc1f92d5178a47e
Btshwanelo <48760912+Btshwanelo@users.noreply.github.com>
2022-09-25 09:03:43 +0200

    added navbar

--- frontend/src/shared/pages/MainPage/index.js
+++ frontend/src/shared/pages/MainPage/index.js
@@ -1,8 +1,23 @@
 import React from "react";
 import PropTypes from "prop-types";
+import "./style.css";
+import { SearchIcon } from "../../assets/icons";
 
 const MainPage = (props) => {
-  return <div>MainPage</div>;
+  return (
+    <div className="main">
+      <div className="navbar">
+        <div className="navbar-logo">Logo</div>
+        <div className="navbar-search">
+          <button>
+            <SearchIcon  />
+          </button>
+          <input type="text" placeholder="Search.." name="search" />
+        </div>
+      </div>
+      <div className="cards">Cards</div>
+    </div>
+  );
 };
 
 MainPage.propTypes = {};
