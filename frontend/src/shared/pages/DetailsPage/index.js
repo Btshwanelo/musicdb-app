2a4acefcc8d33a672f35b1fadd6de1d30661f89f
Btshwanelo <48760912+Btshwanelo@users.noreply.github.com>
2022-09-25 19:28:25 +0200

    added details page components

--- frontend/src/shared/pages/DetailsPage/index.js
+++ frontend/src/shared/pages/DetailsPage/index.js
@@ -1,8 +1,82 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { SearchIcon } from "../../assets/icons";
+import "./style.css";
 
 const DetailsPage = (props) => {
-  return <div>index</div>;
+  return (
+    <div className="detail-page">
+      <div className="navbar">
+        <div className="navbar-logo">LOGO</div>
+        <div className="navbar-search">
+          <button>
+            <SearchIcon />
+          </button>
+          <input type="text" placeholder="Search.." name="search" />
+        </div>
+      </div>
+      <div className="main-section">
+        <div className="artist-section">
+          <div className="artist-info">
+            <img src="https://via.placeholder.com/150" alt="Avatar" />
+          </div>
+          <div className="top-tracks">
+            <h4>Top tracks</h4>
+            <ul>
+              <li>
+                1. You are my lady remastered <span>03:00</span>
+              </li>
+              <li>
+                2. You are my lady remastered <span>03:00</span>
+              </li>
+              <li>
+                3. You are my lady remastered <span>03:00</span>
+              </li>
+              <li>
+                4. You are my lady remastered <span>03:00</span>
+              </li>
+              <li>
+                5. You are my lady remastered <span>03:00</span>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="albums-section">
+          <h2>Albums</h2>
+          <div className="album-cards">
+            <div className="cards-card">
+              <img src="https://via.placeholder.com/150" alt="Avatar" />
+              <div className="cards-container">
+                <h4>Impact of Intrisic</h4>
+                <p>2018</p>
+              </div>
+            </div>
+            <div className="cards-card">
+              <img src="https://via.placeholder.com/150" alt="Avatar" />
+              <div className="cards-container">
+                <h4>Impact of Intrisic</h4>
+                <p>2018</p>
+              </div>
+            </div>
+            <div className="cards-card">
+              <img src="https://via.placeholder.com/150" alt="Avatar" />
+              <div className="cards-container">
+                <h4>Impact of Intrisic</h4>
+                <p>2018</p>
+              </div>
+            </div>
+            <div className="cards-card">
+              <img src="https://via.placeholder.com/150" alt="Avatar" />
+              <div className="cards-container">
+                <h4>Impact of Intrisic</h4>
+                <p>2018</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 DetailsPage.propTypes = {};
