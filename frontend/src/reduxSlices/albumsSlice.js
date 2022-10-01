423161717968de1d96379f07c4fd6fcbc839d801
Btshwanelo <48760912+Btshwanelo@users.noreply.github.com>
2022-10-01 09:10:00 +0200

    added albumsSlice

---
+++ frontend/src/reduxSlices/albumsSlice.js
@@ -0,0 +1,48 @@
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+const initialState = {
+  albumsError: null,
+  albumsLoading: false,
+  albums: []
+};
+
+export const getAlbums = createAsyncThunk('artist/getAlbums', async (artistId, thunkAPI) => {
+  try {
+    const resp = await axios(`/artist/${artistId}/albums`);
+    return resp.data;
+  } catch (error) {
+    return thunkAPI.rejectWithValue('something went wrong');
+  }
+});
+
+const albumsSlice = createSlice({
+  name: 'albums',
+  initialState,
+  reducers: {
+    clearAlbums: (state) => {
+      state.albums = [];
+    }
+  },
+  extraReducers: {
+    [getAlbums.pending]: (state) => {
+      state.albumsError = null;
+      state.albumsLoading = true;
+      state.albums = [];
+    },
+    [getAlbums.fulfilled]: (state, action) => {
+      state.albumsError = null;
+      state.albumsLoading = false;
+      state.albums = action.payload.data;
+    },
+    [getAlbums.rejected]: (state) => {
+      state.albumsError = 'Something went wrong';
+      state.albumsLoading = false;
+      state.albums = [];
+    }
+  }
+});
+
+export const { clearAlbums } = albumsSlice.actions;
+
+export default albumsSlice.reducer;
